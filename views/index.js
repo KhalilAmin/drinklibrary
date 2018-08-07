90315ae8112f1afc067161d5d22f1259d95db7ff
erin hubbard <erinhubbard1981@gmail.com>
2018-08-06 20:25:00 -0400

    front end first draft

---
+++ views/index.js
@@ -0,0 +1,33 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function showSearchForm() {
+  document.getElementById("search").classList.toggle("show");
+}
+
+function showSearchResults() {
+  document.getElementById("searchResultsDisplay").classList.toggle("show");
+}
+
+function closeResultsDiv() {
+  document.getElementById("searchResultsDisplay").classList.toggle("show");
+}
+
+function closeSearchDiv() {
+  document.getElementById("search").classList.toggle("show");
+}
