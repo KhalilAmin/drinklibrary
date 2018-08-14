2c0d20f584f6e54337e1f1991a20bf1b4c57b87e
khalil <kamin86@gmail.com>
2018-08-14 02:25:56 -0400

    minor changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", "ejs");
 
